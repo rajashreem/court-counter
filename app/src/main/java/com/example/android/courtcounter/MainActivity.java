020afbf888a8b9dbbee8d9c21dac9209416866b0
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-08 14:46:02 +0530

    Using the correct scoring model for team B

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,25 +15,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
-    }
-
     public void addThreePointsForTeamA(View view) {
         scoreTeamA += 3;
         displayForTeamA(scoreTeamA);
     }
-
 
     public void addTwoPointsForTeamA(View view) {
         scoreTeamA += 2;
         displayForTeamA(scoreTeamA);
     }
 
-
     public void addOnePointsForTeamA(View view) {
         scoreTeamA += 1;
         displayForTeamA(scoreTeamA);
+    }
+
+    public void addThreePointsForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoPointsForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOnePointsForTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    private void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    private void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
