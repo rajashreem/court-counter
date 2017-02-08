7d39dec6ab639c5a2de985a3b10c6d7a8df99b8c
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-08 14:15:09 +0530

    Adding the basic scoring model for showing the score when different buttons are clicked

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreePointsForTeamA(View view) {
+        displayForTeamA(3);
+    }
+
+
+    public void addTwoPointsForTeamA(View view) {
+        displayForTeamA(2);
+    }
+
+
+    public void addOnePointsForTeamA(View view) {
+        displayForTeamA(1);
     }
 }
